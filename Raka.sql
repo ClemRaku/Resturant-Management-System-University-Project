0d26d0c5b29b3f16b9609ce95f8dd55caf09a20e
Clement Raka De Costa <clement1raka@gmail.com>
2025-11-16 22:47:11 +0600

    Renamed and deleted a column from recipe item table.

--- Raka.sql
+++ Raka.sql
@@ -107,11 +107,10 @@ DROP TABLE IF EXISTS `recipe_item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recipe_item` (
-  `id` int NOT NULL,
-  `menu_item_id` int NOT NULL,
+  `recipe_item_ID` int NOT NULL,
+  `menu_id` int NOT NULL,
   `inventory_id` int NOT NULL,
-  `quantity_required` decimal(10,2) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `quantity_required` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -133,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-16 22:39:33
+-- Dump completed on 2025-11-16 22:46:49
