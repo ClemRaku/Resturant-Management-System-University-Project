95dd21dd03e3396c4067ea12f8daa4c4a01dbaee
Clement Raka De Costa <clement1raka@gmail.com>
2025-11-17 17:16:53 +0600

    Added Accounts table
    also linked email to employess table

--- Raka.sql
+++ Raka.sql
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accounts` (
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `phone_no` int DEFAULT NULL,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone_no_UNIQUE` (`phone_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `customer`
 --
 
@@ -59,9 +86,14 @@ CREATE TABLE `employees` (
   `address` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `job_position` varchar(55) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `employement_date` date DEFAULT NULL,
-  `email` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `availability` tinyint DEFAULT NULL,
-  PRIMARY KEY (`employee_id`)
+  PRIMARY KEY (`employee_id`),
+  KEY `email_idx` (`email`),
+  KEY `fk_employee_email_idx` (`email`),
+  KEY `fk_employee_phone_no_acc` (`phone_no`),
+  CONSTRAINT `fk_employee_email_acc` FOREIGN KEY (`email`) REFERENCES `accounts` (`email`),
+  CONSTRAINT `fk_employee_phone_no_acc` FOREIGN KEY (`phone_no`) REFERENCES `accounts` (`phone_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -299,4 +331,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-17  3:22:01
+-- Dump completed on 2025-11-17 17:16:30
