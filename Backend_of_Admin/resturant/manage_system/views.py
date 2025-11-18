7b89492d6de5fe45dd797fa1f45cb23a6dbd11ba
Clement Raka De Costa <clement1raka@gmail.com>
2025-11-19 03:31:33 +0600

    the DELETE button from menu page doesn't yet work.

--- Backend_of_Admin/resturant/manage_system/views.py
+++ Backend_of_Admin/resturant/manage_system/views.py
@@ -13,6 +13,7 @@ def admin_menu(request):
     mycursor = mydb.cursor()
     
     if request.GET.get("new_menu_id"):
+        
         IDmenu_str = request.GET.get("new_menu_id")
         IDmenu = int(IDmenu_str)
         menu_name = request.GET.get("new_menu_name")
@@ -33,27 +34,38 @@ def admin_menu(request):
         
         
         #doing the editing bit now
-        if request.GET.get("edit_menu_id"):
-            edit_id = int(request.GET.get("edit_menu_id"))
-            edit_name = request.GET.get("eidit_menu_name")
-            edit_category = int(request.GET.get("edit_catagory"))
-            edit_description = request.GET.get("edit_description")
-            edit_ingredients = request.GET.get("edit_ingredients")
-            edit_price = float(request.GET.get("eidit_price"))
-            edit_prep = int(request.GET.get("edit_preparation_time"))
-            
-            select_menu_items = "UPDATE menu SET name = %s, description = %s, price = %s, category_id = %s, ingredients = %s, preparation_time = %s WHERE menu_id = %s"
-            dt = (
-                edit_name, 
-                edit_description, 
-                edit_price, 
-                edit_category, 
-                edit_ingredients, 
-                edit_prep, 
-                edit_id 
-            ) 
-            mycursor.execute(sql, data)
-            mydb.commit()       
+    if request.GET.get("edit_menu_id"):
+        edit_id = request.GET.get("edit_menu_id")
+        edit_id_int = int(edit_id)
+        
+        edit_name = request.GET.get("eidit_menu_name")
+        
+        edit_category = request.GET.get("edit_catagory")
+        edit_catagoryID = int(edit_category)
+        
+        edit_description = request.GET.get("edit_decription")
+        
+        edit_ingredients = request.GET.get("edit_ingredients")
+        
+        edit_price = request.GET.get("eidit_price")
+        edit_price_float = float(edit_price)
+        
+        edit_prep = request.GET.get("edit_preparation_time")
+        edit_prep_int = int(edit_prep)
+        
+        
+        select_menu_items = "UPDATE menu SET name = %s, description = %s, price = %s, category_id = %s, ingredients = %s, preparation_time = %s WHERE menu_id = %s"
+        dt = (
+            edit_name, 
+            edit_description, 
+            edit_price_float, 
+            edit_catagoryID, 
+            edit_ingredients, 
+            edit_prep_int, 
+            edit_id_int 
+        ) 
+        mycursor.execute(select_menu_items, dt)
+        mydb.commit()       
         
     selectALL_menu_items = "SELECT menu_id, name, category_id, description, ingredients, preparation_time, price FROM menu"
     mycursor.execute(selectALL_menu_items)
@@ -62,6 +74,13 @@ def admin_menu(request):
     context = {'menu_items' : items_from_menu}
     
     mycursor.close()
+
     
     return render (request, 'adminmenu.html', context)
+
+
+#def signup_signin(request):
+    
 # Create your views here.
+
+
