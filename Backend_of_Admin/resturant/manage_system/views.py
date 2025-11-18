b796f63b7983f70b27112e26946796f712b7534b
Clement Raka De Costa <clement1raka@gmail.com>
2025-11-18 17:48:48 +0600

    connected HTML, connected MYSQL80. made sql scripts to insert the values in the database.

--- Backend_of_Admin/resturant/manage_system/views.py
+++ Backend_of_Admin/resturant/manage_system/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render, redirect
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'raka',
+    database = 'resturant'
+)
+
+
+def admin_menu(request):
+    mycursor = mydb.cursor()
+    
+    
+    IDmenu_str = request.GET.get("add_id")
+    IDmenu = int(IDmenu_str)
+    menu_name = request.GET.get("new_menu_name")
+    categoryID = request.GET.get("new_catagory")
+    ingredients = request.GET.get("new_ingredients")
+    prep_time_str = request.GET.get("preparation_time")
+    prep_time_int = int(prep_time_str)
+    price_str = request.GET.get("new_price")
+    price = float(price_str)
+    description = request.GET.get("new_description")
+    
+    
+    sql = "INSERT INTO menu (menu_id, name, description, price, category_id, ingredients, preparation_time) values (%s, %s, %s, %s, %s, %s, %s)"
+    data = (IDmenu, menu_name, description, price, categoryID, ingredients, prep_time_int)
+    mycursor.execute(sql, data)
+    mydb.commit()
+    mycursor.close()
+    
+    return render (request, 'adminmenu.html')
 # Create your views here.
